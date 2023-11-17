74e1832d23fb5dd7dd89be75b18344a85a01a0a8
Ethem ÇAĞLAR <142922479+EthemCaglar@users.noreply.github.com>
2023-11-17 19:54:55 +0300

    Update odev9-11.sql

--- odev9-11.sql
+++ odev9-11.sql
@@ -1,8 +1,8 @@
---SELECT city.city, country.country FROM city JOIN country ON city.country_id = country.country_id;
+--SELECT city.city, country.country FROM city INNER JOIN country ON city.country_id = country.country_id;
 
---SELECT payment.payment_id, customer.first_name, customer.last_name FROM payment JOIN customer ON customer.customer_id = payment.customer_id;
+--SELECT payment.payment_id, customer.first_name, customer.last_name FROM payment INNER JOIN customer ON customer.customer_id = payment.customer_id;
 
---SELECT rental.rental_id, customer.first_name, customer.last_name FROM customer JOIN rental ON customer.customer_id = rental.customer_id;
+--SELECT rental.rental_id, customer.first_name, customer.last_name FROM customer INNER JOIN rental ON customer.customer_id = rental.customer_id;
 
 --SELECT city.city, country.country FROM city LEFT JOIN country ON city.country_id = country.country_id;
 
